202ee4497d783350c7c83bab7938f1876c1f1f28
Tim Koar <koartimothy@gmail.com>
2019-09-09 15:30:20 -0400

    added cards

--- HelloWorld.js
+++ HelloWorld.js
@@ -1,12 +1,46 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, ScrollView } from 'react-native';
+import { Card, ListItem, Button, Icon } from 'react-native-elements'
 
 export default class HelloWorld extends Component {
+
+  state = {
+    charities: []
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:3000/api/v1/charities")
+      .then(rsp => rsp.json())
+      .then(rsp => {
+        this.setState({
+          charities: rsp
+        })
+      }).catch(function(error) {
+        console.log('there has been an error in your fetch operation:' + error.message);
+      });
+  }
+
   render() {
     return (
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Text>Hello, world!</Text>
-      </View>
+      <ScrollView containerStyle={styles.container}>
+      {this.state.charities.map(charity => {
+        return(
+          <Card>
+          <Text>{charity.charityName}</Text>
+          </Card>
+        )
+      })}
+      </ScrollView>
     );
   }
 }
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+      paddingVertical: 20,
+    },
+  });

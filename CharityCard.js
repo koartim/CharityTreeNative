454495ec4d9608ba905d374307546a6a93dbfbbf
Tim Koar <koartimothy@gmail.com>
2019-09-13 15:44:22 -0400

    added image to cards

--- CharityCard.js
+++ CharityCard.js
@@ -1,15 +1,18 @@
 import React from 'react'
-import { StyleSheet, Text, ScrollView, View } from 'react-native';
-import { Card, ListItem, Button, Image, Header } from 'react-native-elements'
+import { StyleSheet, Text, ScrollView, View, Image } from 'react-native';
+import { Card, ListItem, Button, Header } from 'react-native-elements'
 
 class CharityCard extends React.Component {
 
   render() {
-    console.log(this.props)
+    let pic = {
+      uri: this.props.charity.cause.image
+    }
       return(
         <ScrollView style={styles.container}>
           <Card>
             <Text style={styles.titleText}>{this.props.charity.charityName}</Text>
+            <Image source={pic} style={{width: 75, height: 75}}/>
             <Button title="view"></Button>
           </Card>
         </ScrollView>

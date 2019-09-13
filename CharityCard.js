f67f51e5b60a7b3f719752b0847a44c19893739d
Tim Koar <koartimothy@gmail.com>
2019-09-13 15:27:51 -0400

    configured card styling

--- CharityCard.js
+++ CharityCard.js
@@ -1,15 +1,15 @@
 import React from 'react'
-import { StyleSheet, Text, ScrollView } from 'react-native';
-import { Card, ListItem, Button, Image } from 'react-native-elements'
+import { StyleSheet, Text, ScrollView, View } from 'react-native';
+import { Card, ListItem, Button, Image, Header } from 'react-native-elements'
 
 class CharityCard extends React.Component {
 
   render() {
     console.log(this.props)
       return(
-        <ScrollView>
+        <ScrollView style={styles.container}>
           <Card>
-            <Text>{this.props.charity.charityName}</Text>
+            <Text style={styles.titleText}>{this.props.charity.charityName}</Text>
             <Button title="view"></Button>
           </Card>
         </ScrollView>
@@ -19,9 +19,11 @@ class CharityCard extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#7CA1B4',
-    alignItems: 'center',
-    justifyContent: 'center'
+    backgroundColor: '#7CA1B4'
+  },
+  titleText: {
+    fontSize: 20,
+    fontWeight: 'bold'
   },
 });
 
